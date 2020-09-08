11bcc264298ae650db2988b60c51bc15693af7b6
MateusPompelli <mateus-pompelli@hotmail.com>
2020-09-07 23:12:25 -0300

    test

--- trabalho/src/app/app.component.ts
+++ trabalho/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trabalho';
+  public formLogin: FormGroup;
+  public array = environment.test;
+
+  public time 
+  
+  constructor(
+    //private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.buildForm()
+  }
+
+  public buildForm() {
+    
+  }
+
+  public test(){
+    console.log('soifhsoidfj')
+    console.log(this.array.length)
+    
+
+  }
+
 }
